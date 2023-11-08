c2b73d0647450d56bb623bbce0c3038d6e1ec300
Igor Dalepiane da Costa <igordalepiane@gmail.com>
2023-11-07 23:37:50 -0300

    Version 1.0.0 (#1)
    * wip
    
    * wip
    
    * docs: 📚️ add requirements.txt, update readme
    
    * fix: 🐛 save instead of showing plot
    
    * feat: ✨ add argparse
    
    * fix: args
    
    * fix: file name
    
    * docs: 📚️ update readme
    
    * feat: translations, 100 bars
    
    * fix: 🐛 save seed to file
    
    * feat: range on chi-squared test
    
    * docs: readme updated with source codes
    
    ---------
    
    Co-authored-by: LucasFell <lucas.fell@partner.bmw.com.br>

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# otim-rng-tests
+# Otimização de Algoritmos - PPGES 2023/02
+
+#### Capítulo 16.7 (Random number generation) do livro "The Algorithm Design Manual", de Skiena
+
+Alunos: [Igor Costa](https://github.com/IgorDalepiane) e [Lucas Fell](https://github.com/fell-lucas)
+
+### Dependências:
+
+- memory-profiler==0.61.0
+- matplotlib==3.8.1
+- scipy==1.11.3
+- numpy==1.26.1
+- argparse==1.4.0
+
+Para instalar, basta executar o comando:
+
+```bash
+pip install -r requirements.txt
+```
+#### Códigos fontes
+
+ - Algoritmo LCG: https://github.com/rossilor95/lcg-python/blob/main/lcg.py
+ - Algoritmo Mersenne Twister: https://github.com/python/cpython/blob/3.12/Lib/random.py
+
+### Execução:
+
+Para executar o programa, basta executar o comando:
+
+```bash
+python3 main.py
+```
+
+Opcionalmente, você pode passar os seguntes argumentos:
+
+- `--lcg_modulus`: Módulo 'm' do LCG, padrão: 4_294_967_296
+- `--lcg_multiplier`: Multiplicador 'a' do LCG, padrão: 594_156_893
+- `--lcg_increment`: Incremento 'c' do LCG, padrão: 0
+- `--seed`: Semente inicial dos algoritmos LCG e MT, padrão: gerada aleatoriamente
+- `--count`: Quantidade de números aleatórios a serem gerados, padrão: testes de 1000, 100000 e 1000000 números
+- `--max_range`: Valor máximo dos números aleatórios gerados, padrão: 1000000
+
+Exemplo:
+
+```bash
+python3 main.py --lcg_modulus 4294967296 --lcg_multiplier 594156893 --lcg_increment 0 --seed 123456789 --count 1000 --max_range 1000000
+```
+
+### Resultados:
+
+Os resultados serão salvos no diretório `plots/` com o nome `<seed>_<count>.png`.
---
+++ main.py
@@ -0,0 +1,159 @@
+# import pure_python_random_library as py_random
+import argparse
+import py_random_source_code as py_random
+from typing import List
+import lcg  # Import the lcg.py module
+import time
+import memory_profiler
+from matplotlib import pyplot as plt
+from scipy import stats
+import numpy as np
+
+def print_statistics(case_name, time_taken, memory_used, chi_stat, chi_p, auto_corr):
+    print(f"{case_name} Test Case:")
+    print(f"  Tempo percorrido: {time_taken:.6f}s")
+    print(f"  Memória utilizada: {memory_used:.6f} MiB")
+    print(f"  Resultados estatísticos:")
+    print(f"    Teste Qui-Quadrado: {chi_stat}")
+    print(f"    P-Value: {chi_p}")
+    print(f"    Coeficiente de autocorrelação: {auto_corr}\n")
+
+# Mersenne Twister Implementation
+class MersenneTwister:
+    def __init__(self, seed=None):
+        self.random_instance = py_random.Random(seed)
+    
+    def generate_sequence(self, n_samples: int) -> List[float]:
+        return [self.random_instance.random() for _ in range(n_samples)]
+
+def run_test_cases(lcg_multiplier, lcg_modulus, lcg_increment, seed, maxRange, count=None):
+    # Initialize the Mersenne Twister with a random seed
+    mt = MersenneTwister(seed)
+    
+    test_cases = [
+        {"n_numbers": 1000, "range": (0, 100)},
+        {"n_numbers": 100000, "range": (1, 10000)},
+        {"n_numbers": 1000000, "range": (1, 1000000)},
+    ]
+
+    if count is not None:
+        test_cases = [{"n_numbers": count, "range": (1, maxRange)}]
+    
+    for case in test_cases:
+        # Memory and time measurement start for LCG
+        start_time_lcg = time.perf_counter()
+        mem_usage_start_lcg = memory_profiler.memory_usage()
+
+        # Generating LCG numbers
+        lcg_raw_floats = lcg.rand_float_samples(case["n_numbers"], lcg_modulus, lcg_multiplier, lcg_increment, seed)
+        lcg_numbers = [int(num * (case["range"][1] - case["range"][0])) + case["range"][0]
+                       for num in lcg_raw_floats]
+        
+        # Memory and time measurement end for LCG
+        mem_usage_end_lcg = memory_profiler.memory_usage()
+        end_time_lcg = time.perf_counter()
+        
+        # Memory and time measurement start for MT
+        start_time_mt = time.perf_counter()
+        mem_usage_start_mt = memory_profiler.memory_usage()
+        
+        # Generating MT numbers
+        mt_raw_floats = mt.generate_sequence(case["n_numbers"])
+        mt_numbers = [int(num * (case["range"][1] - case["range"][0])) + case["range"][0]
+                      for num in mt_raw_floats]
+        
+        # Memory and time measurement end for MT
+        mem_usage_end_mt = memory_profiler.memory_usage()
+        end_time_mt = time.perf_counter()
+        
+        # Calculate performance metrics
+        time_taken_lcg = end_time_lcg - start_time_lcg
+        mem_used_lcg = max(mem_usage_end_lcg) - min(mem_usage_start_lcg)
+        
+        time_taken_mt = end_time_mt - start_time_mt
+        mem_used_mt = max(mem_usage_end_mt) - min(mem_usage_start_mt)
+
+        # Chi-squared test for uniformity for LCG
+        observed_frequencies, _ = np.histogram(lcg_numbers, bins=100, range=case["range"])
+        expected_frequencies = np.full_like(observed_frequencies, len(lcg_numbers)/100)
+        chi_square_statistic_lcg, p_value_lcg = stats.chisquare(observed_frequencies, expected_frequencies)
+        autocorr_coefficient_lcg = np.corrcoef(lcg_numbers[:-1], lcg_numbers[1:])[0, 1]
+
+        # Chi-squared test for uniformity for MT
+        observed_frequencies, _ = np.histogram(mt_numbers, bins=100, range=case["range"])
+        expected_frequencies = np.full_like(observed_frequencies, len(mt_numbers)/100)
+        chi_square_statistic_mt, p_value_mt = stats.chisquare(observed_frequencies, expected_frequencies)
+        autocorr_coefficient_mt = np.corrcoef(mt_numbers[:-1], mt_numbers[1:])[0, 1]
+
+        # Print results
+        print_statistics('LCG', time_taken_lcg, mem_used_lcg, chi_square_statistic_lcg, p_value_lcg, autocorr_coefficient_lcg)
+        print_statistics('MT', time_taken_mt, mem_used_mt, chi_square_statistic_mt, p_value_mt, autocorr_coefficient_mt)
+        print('-' * 50)
+        
+        # Plotting the results for visual comparison
+        plt.figure(figsize=(12, 6))
+
+        plt.subplot(1, 2, 1)
+        plt.hist(lcg_numbers, bins=100, alpha=0.7, label='LCG')
+        plt.title('Distribuição do LCG')
+        plt.xlabel('Número')
+        plt.ylabel('Frequência')
+
+        plt.subplot(1, 2, 2)
+        plt.hist(mt_numbers, bins=100, alpha=0.7, label='Mersenne Twister')
+        plt.title('Distribuição do Mersenne Twister')
+        plt.xlabel('Número')
+        plt.ylabel('Frequência')
+
+        plt.tight_layout()
+        plt.savefig(f"plots/{seed}_{case['n_numbers']}.png")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="RNG Test Cases comparing LCG and MT algorithms")
+
+    parser.add_argument(
+        "--lcg_modulus", type=int, default=2**32, help="Multiplicador 'a' do LCG"
+    )
+    parser.add_argument(
+        "--lcg_multiplier", type=int, default=594_156_893, help="Módulo 'm' do LCG"
+    )
+    parser.add_argument(
+        "--lcg_increment", type=int, default=0, help="Incremento 'c' do LCG"
+    )
+    parser.add_argument(
+        "--seed", type=int, required=False, help="Seed inicial dos algoritmos LCG e MT"
+    )
+    parser.add_argument(
+        "--count", type=int, required=False, help="Número total de números aleatórios a serem gerados"
+    )
+    parser.add_argument(
+        "--max_range", type=int, default=1000000, help="Abrangência dos números aleatórios gerados"
+    )
+
+    args = parser.parse_args()
+
+    if args.lcg_multiplier >= args.lcg_modulus or args.lcg_increment >= args.lcg_modulus:
+        raise ValueError("lcg_multiplier e lcg_increment devem ser menores que lcg_modulus")
+
+    # Access the LCG parameters from the args namespace
+    lcg_multiplier = args.lcg_multiplier
+    lcg_modulus = args.lcg_modulus
+    lcg_increment = args.lcg_increment
+    seed = args.seed or py_random.Random().randint(0, 2**32 - 1)
+    count = args.count
+    maxRange = args.max_range
+
+    # log
+    print(f"LCG Multiplier: {lcg_multiplier}")
+    print(f"LCG Increment: {lcg_increment}")
+    print(f"LCG Modulus: {lcg_modulus}")
+    print(f"Seed: {seed}")
+    if count is not None:
+        print(f"Count: {count}")
+        print(f"Range: 1 - {maxRange}")
+
+    run_test_cases(lcg_multiplier, lcg_modulus, lcg_increment, seed, maxRange, count)
+    
+if __name__ == "__main__":
+    main()
